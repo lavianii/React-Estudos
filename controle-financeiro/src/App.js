02fa4da37f74b4077d5feb46a7c2748089b9b012
lavianii <jonaslavi02@gmail.com>
2023-02-21 20:01:32 -0300

    refatorando

---
+++ controle-financeiro/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+      <div>
+        <h1>Teste</h1>
+      </div>
+  );
+}
+
+
---
+++ controle-financeiro/src/components/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default Global = createGlobalStyle`
+
+  body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+    background-color: #f2f2f2;
+  }
+`;
