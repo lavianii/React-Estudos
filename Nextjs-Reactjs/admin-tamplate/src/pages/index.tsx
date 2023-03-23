6bbc7477552a88c7263cb85115f07e8b8cb9a748
lavianii <jonaslavi02@gmail.com>
2023-03-23 15:48:59 -0300

    initial commit

---
+++ Nextjs-Reactjs/admin-tamplate/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/template/Layout";
+
+export default function Home() {
+  return (
+    <Layout titulo="Página inical" subtitulo="Estamos construindo um tampla Adm">
+      Teste
+    </Layout>
+  )
+}
